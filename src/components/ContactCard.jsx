3aea0cefd8746e08c5c6ee6c86b0ae2be699a2dc
TonyB <buiductho195@gmail.com>
2024-07-19 00:32:24 +0700

    Update link target
    Signed-off-by: TonyB <buiductho195@gmail.com>

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,23 +3,26 @@ import { faLocationDot, faEnvelope, faGlobe } from '@fortawesome/free-solid-svg-
 
 const ContactCard = () => {
   const contactData = [
-    { id: 'mail', value: 'buiductho195@gmail.com', icon: <EnvelopeIcon className="size-5 min-w-5" /> },
-    { id: 'website', value: 'tonybthedev.com', icon: <GlobeAltIcon className="size-5 min-w-5" /> },
     {
       id: 'mail',
       value: 'buiductho195@gmail.com',
       icon: <FontAwesomeIcon className="min-h-4 min-w-4" icon={faEnvelope} />,
+      onClick: () => (window.location.href = 'mailto:buiductho195@gmail.com'),
+      customClassNames: 'cursor-pointer',
     },
     {
       id: 'website',
       value: 'tonybthedev.com',
       icon: <FontAwesomeIcon className="min-h-4 min-w-4" icon={faGlobe} />,
+      onClick: () => window.open('https://tonybthedev.com', '_blank'),
+      customClassNames: 'cursor-pointer',
     },
     {
       id: 'address',
       value: 'Khuong Dinh, Ha Dinh, Thanh Xuan, Hanoi',
-      icon: <MapPinIcon className="size-5 min-w-5" />,
       icon: <FontAwesomeIcon className="min-h-4 min-w-4" icon={faLocationDot} />,
+      onClick: undefined,
+      customClassNames: undefined,
     },
   ];
 
@@ -28,9 +31,10 @@ const ContactCard = () => {
       <div className="text-2xl font-semibold">Contact</div>
       <div className="mx-8 flex flex-col gap-3">
         {contactData.map((contact) => (
-          <div key={contact.id} className="flex items-center gap-3 text-xl md:text-base">
           <div
             key={contact.id}
+            onClick={contact.onClick}
+            className={`flex items-center gap-3 text-xl md:text-base ${contact.customClassNames}`}
           >
             {contact.icon}
             <span>{contact.value}</span>
