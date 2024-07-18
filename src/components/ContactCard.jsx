626384f66183ef58740425f2df63298a28fb8bed
TonyB <buiductho195@gmail.com>
2024-07-19 00:31:23 +0700

    Change icons
    Signed-off-by: TonyB <buiductho195@gmail.com>

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,13 +1,25 @@
-import { EnvelopeIcon, GlobeAltIcon, MapPinIcon, PhoneIcon } from '@heroicons/react/24/solid/index.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot, faEnvelope, faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 const ContactCard = () => {
   const contactData = [
     { id: 'mail', value: 'buiductho195@gmail.com', icon: <EnvelopeIcon className="size-5 min-w-5" /> },
     { id: 'website', value: 'tonybthedev.com', icon: <GlobeAltIcon className="size-5 min-w-5" /> },
     {
+      id: 'mail',
+      value: 'buiductho195@gmail.com',
+      icon: <FontAwesomeIcon className="min-h-4 min-w-4" icon={faEnvelope} />,
+    },
+    {
+      id: 'website',
+      value: 'tonybthedev.com',
+      icon: <FontAwesomeIcon className="min-h-4 min-w-4" icon={faGlobe} />,
+    },
+    {
       id: 'address',
       value: 'Khuong Dinh, Ha Dinh, Thanh Xuan, Hanoi',
       icon: <MapPinIcon className="size-5 min-w-5" />,
+      icon: <FontAwesomeIcon className="min-h-4 min-w-4" icon={faLocationDot} />,
     },
   ];
 
@@ -17,6 +29,9 @@ const ContactCard = () => {
       <div className="mx-8 flex flex-col gap-3">
         {contactData.map((contact) => (
           <div key={contact.id} className="flex items-center gap-3 text-xl md:text-base">
+          <div
+            key={contact.id}
+          >
             {contact.icon}
             <span>{contact.value}</span>
           </div>
