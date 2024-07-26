90e07763bf2aed9cdebd309c87b255527d307e94
TonyB <buiductho195@gmail.com>
2024-07-26 23:59:27 +0700

    Add interests card
    Signed-off-by: TonyB <buiductho195@gmail.com>

---
+++ src/components/InterestsCard.jsx
@@ -0,0 +1,14 @@
+const InterestsCard = () => {
+  return (
+    <div className="flex flex-col justify-center gap-2 py-8">
+      <div className="text-center text-2xl font-semibold">Interests</div>
+      <div className="mx-8 flex flex-col gap-0.5">
+        <div className="flex items-center gap-3 text-xl md:text-base">
+          Reading book and technical blog. Traveling, Running and Cycling
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InterestsCard;
